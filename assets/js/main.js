5ca793703a51d6166382779024e58bf67bef6c87
Julio <julio4003@hotmail.com>
2022-05-19 11:56:47 -0500

    PortafolioV1

---
+++ assets/js/main.js
@@ -0,0 +1,7 @@
+const navbarBtn = document.getElementById('navbarBtn')
+const navbarNav = document.querySelector('.navbar__nav')
+
+navbarBtn.addEventListener('click', function (e) {
+  console.log(navbarNav)
+  navbarNav.classList.toggle('active')
+})
