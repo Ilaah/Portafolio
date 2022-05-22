a3113d1f9ef580a76329667a62c1064e8895dd26
Julio <julio4003@hotmail.com>
2022-05-22 02:09:16 -0500

    DespliegueMenu

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,24 @@
 const navbarBtn = document.getElementById('navbarBtn')
 const navbarNav = document.querySelector('.navbar__nav')
 
-navbarBtn.addEventListener('click', function (e) {
-  console.log(navbarNav)
+const home = document.querySelector('.intro')
+const aboutMe = document.querySelector('.about')
+const skills = document.querySelector('.skill')
+const portfolio = document.querySelector('.portf')
+const contact = document.querySelector('.cont')
+
+// Activar y desactivar el menú hamburguesa
+
+function toggleMenu () {
   navbarNav.classList.toggle('active')
-})
+}
+
+/*Activar*/
+navbarBtn.addEventListener("click", toggleMenu)
+
+/*Desactivar*/
+home.addEventListener("click", toggleMenu)
+aboutMe.addEventListener("click", toggleMenu)
+skills.addEventListener("click", toggleMenu)
+portfolio.addEventListener("click", toggleMenu)
+contact.addEventListener("click", toggleMenu)
